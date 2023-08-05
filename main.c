42aa6b7ce4f6bb58bcfc0e23f20f147a7436075e
Ketan Sharma <ketansharma@Ketans-MacBook-Air.local>
2023-08-05 12:20:07 +0530

    Did the tut:1 by myself, and added comments regarding the same

--- main.c
+++ main.c
@@ -1,116 +1,18 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <errno.h>
-// #include <string.h>
-// #include "shell.h"
-
-// int main(int argc, char **argv)
-// {
-//     char *cmd;
-
-//     do
-//     {
-//         print_prompt1();
-
-//         cmd = read_cmd();
-
-//         if(!cmd)
-//         {
-//             exit(EXIT_SUCCESS);
-//         }
-
-//         if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
-//         {
-//             free(cmd);
-//             continue;
-//         }
-
-//         if(strcmp(cmd, "exit\n") == 0)
-//         {
-//             free(cmd);
-//             break;
-//         }
-
-//         printf("%s\n", cmd);
-
-//         free(cmd);
-
-//     } while(1);
-
-//     exit(EXIT_SUCCESS);
-// }
-
-// char *read_cmd(void)
-// {
-//     char buf[1024];
-//     char *ptr = NULL;
-//     char ptrlen = 0;
-
-//     while(fgets(buf, 1024, stdin))
-//     {
-//         int buflen = strlen(buf);
-
-//         if(!ptr)
-//         {
-//             ptr = malloc(buflen+1);
-//         }
-//         else
-//         {
-//             char *ptr2 = realloc(ptr, ptrlen+buflen+1);
-
-//             if(ptr2)
-//             {
-//                 ptr = ptr2;
-//             }
-//             else
-//             {
-//                 free(ptr);
-//                 ptr = NULL;
-//             }
-//         }
-
-//         if(!ptr)
-//         {
-//             fprintf(stderr, "error: failed to alloc buffer: %s\n", strerror(errno));
-//             return NULL;
-//         }
-
-//         strcpy(ptr+ptrlen, buf);
-
-//         if(buf[buflen-1] == '\n')
-//         {
-//             if(buflen == 1 || buf[buflen-2] != '\\')
-//             {
-//                 return ptr;
-//             }
-
-//             ptr[ptrlen+buflen-2] = '\0';
-//             buflen -= 2;
-//             print_prompt2();
-//         }
-
-//         ptrlen += buflen;
-//     }
-
-//     return ptr;
-// }
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>//this provides errno and defines a lot of values for it, print it using fprintf
 #include<string.h>
 #include"shell.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv){//main needs to implement the REPL loop
     char *cmd;
     do{
-        print_prompt1();//print the prompt
-        cmd = read_cmd();//read the command
+        print_prompt1();//prints the prompt string
+        cmd = read_cmd();//reads the next line of input
         if(!cmd){//if command can't be read, there is some error
             exit(EXIT_SUCCESS);//EXIT_SUCCESS is from stdlib.h
         }
-        if(cmd[0]!='\0' || strcmp(cmd,"\n")==0){//if no com
+        if(cmd[0]=='\0' || strcmp(cmd,"\n")==0){//if no string is input, then do nothing
             free(cmd);
             continue;
         }
@@ -118,6 +20,47 @@ int main(int argc, char** argv){
             free(cmd);
             break;
         }
+        printf("%s",cmd);
+        free(cmd);
     } while(1);
     exit(EXIT_SUCCESS);//EXIT_SUCCESS is from stdlib.h
+}
+
+char *read_cmd(void){
+    //the input is read in 1024 byte chunks 
+    char buf[1024];// this is the length of the buffer that i have
+    char *ptr = NULL;//this is the ptr to the string that i have
+    char ptrlen = 0;//this is the length of that string
+
+    while(fgets(buf,1024,stdin)){//
+        int buflen = strlen(buf); //so that we know the length of the input string
+        
+        if(!ptr){
+            ptr = malloc(buflen + 1);// for the first chunk we using buflen
+        }
+        else{
+            char *ptr2 = realloc(ptr,ptrlen+buflen+1);//for subsequent chunks we use realloc
+            if(ptr2){
+                ptr = ptr2;
+            } else {
+                free(ptr);
+                ptr = NULL;
+            }
+        }
+        if(!ptr){
+            fprintf(stderr, "error: failed to alloc buffer: %s\n", strerror(errno));
+            return NULL;
+        }
+        strcpy(ptr+ptrlen,buf);
+        if(buf[buflen-1] == '\n'){
+            if(buflen==1||buf[buflen-2]!='\\'){
+                return ptr;
+            }
+            ptr[ptrlen+buflen-2] = '\0';
+            buflen -= 2;
+            print_prompt2();
+        }
+        ptrlen += buflen;
+    }
+    return ptr;
 }
--- prompt.c
+++ prompt.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "shell.h"
 
-void print_prompt1(void)
-{
-    fprintf(stderr, "$ ");
+
+//there are 5 types of prompt string: p0 p1 p2 p3 p4
+//ps0 used by bash, hence not used
+//ps1 to ps4 are used to communicate with the user, hence are not used 
+
+//this prints ps1 for single line input
+void print_prompt1(void){
+    fprintf(stderr, "$ ");//stderr is used, because unlike stdout, it is not buffered
 }
 
-void print_prompt2(void)
-{
-    fprintf(stderr, "> ");
+//this prints ps2 for multi line input
+void print_prompt2(void){
+    fprintf(stderr, "> ");//also helps in distinuishing prompt streams and stdout
 }
